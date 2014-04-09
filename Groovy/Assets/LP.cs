0cbfc09d4d33c7ca58cda1fa76cab6a8efa4e503
Robin K <robin.karlsson@bredband2.com>
2014-04-09 15:33:04 +0200

    Basic Game

---
+++ Groovy/Assets/LP.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LP : MonoBehaviour {
+	private Vector3 Rot;
+	public GameObject Obj;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Rot.x = 0.0f;
+		Rot.y = 0.0025f;
+		Rot.z = 0.0f;
+		iTween.RotateBy(Obj, Rot, 0.0f);
+	}
+}
